6bf279e958f552c7bd934b7dd87b4e983744362f
JamesNYim <101849565+JamesNYim@users.noreply.github.com>
2024-06-20 17:05:30 -0700

    Unique user registration check

--- backend/database.sql
+++ backend/database.sql
@@ -1,5 +1,4 @@
 CREATE DATABASE yap;
-
 CREATE TABLE users(
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(255),
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
-const express = require("express");
-const app = express();
+const express = require("express"); const app = express();
 const cors = require("cors");
 const pool = require("./db");
 
@@ -38,11 +37,11 @@ app.get("/users", async (req, res) => {
     }
 });
 
-// Get a user //
-app.get("/users/:id", async (req, res) => {
+// Get a user by username //
+app.get("/users/:username", async (req, res) => {
     try {
-        const { id } = req.params;
-        const users = await pool.query("SELECT * FROM users WHERE user_id = $1", [id]);
+        const { username } = req.params;
+        const users = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
         res.json(users.rows[0]);
         console.log(req.params);
     }
@@ -75,4 +74,45 @@ app.delete("/users/:id", async (req, res) => {
         [id]
     );
     res.json("User was deleted");
+});
+
+// Checking if a username can be registered
+app.post('/check-user-availability', async (req, res) => {
+    const { username } = req.body;
+    try {
+        const userResult = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+
+         
+        // If a Username is already taken
+        if (userResult.rows.length > 0) {
+            return res.status(200).json({ availability: false, field: 'username', message: 'Username is already taken' }); 
+        } 
+        // If username can be registered 
+        return res.status(200).json({availability: true}); 
+    }
+    catch (e) {
+        console.log('Error checking availability', e);
+        res.status(500).send('Server Error');
+    }
+});
+
+// Checking if an email can be registered
+app.post('/check-email-availability', async (req, res) => {
+    const { email } = req.body;
+    try {
+        const emailResult = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        console.log('Email query: ', emailResult.rows.length);
+        
+        // If an Email is already registered
+        if (emailResult.rows.length > 0) {
+            return res.status(200).json({ availability: false, field: 'email', message: 'Email is already registered' });
+        }
+        
+        // If email can be registered
+        return res.status(200).json({availability: true});
+    }
+    catch (e) {
+        console.log('Error checking availability', e.message);
+        res.status(500).send('Server Error');
+    }
 });
