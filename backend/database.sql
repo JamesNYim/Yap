f82d1e30f9a6ae9aa6120e2c607dbe0ef3b186cc
JamesNYim <101849565+JamesNYim@users.noreply.github.com>
2024-08-06 14:30:15 -0700

    Added posts table

--- backend/database.sql
+++ backend/database.sql
@@ -1,7 +1,21 @@
 CREATE DATABASE yap;
 CREATE TABLE users(
     user_id SERIAL PRIMARY KEY,
-    username VARCHAR(255),
-    email VARCHAR(255),
+    username VARCHAR(255) UNIQUE,
+    email VARCHAR(255) UNIQUE,
     password VARCHAR(255)
 );
+CREATE TABLE posts(
+    post_id SERIAL PRIMARY KEY,
+    creator_id INT NOT NULL, 
+    FOREIGN KEY (creator_id) REFERENCES users(user_id),
+    creator_name VARCHAR(255) NOT NULL, 
+    FOREIGN KEY (creator_name) REFERENCES users(username),
+    content VARCHAR(255)
+);
+
+CREATE TABLE comments(
+    comment_id SERIAL PRIMARY KEY,
+    comment VARCHAR(255)
+);
+
